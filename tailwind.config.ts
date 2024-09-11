c34d5106f9f8b723626e71548262d3db225aeb16
SaD-n-SoliD <yodas-worker@yandex.ru>
2024-09-11 16:08:38 +0300

    tailwind config: leading-tight 1.25 -> 1.2

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,9 @@ const config: Config = {
 				"gradient-conic":
 					"conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
 			},
+			lineHeight: {
+				tight: '1.2'
+			}
 		},
 	},
 	plugins: [],
