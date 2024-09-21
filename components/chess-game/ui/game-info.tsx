dadf383dca56480fce8e32f3418bbe1dd7693240
SaD-n-SoliD <yodas-worker@yandex.ru>
2024-09-21 16:52:02 +0300

    prop rename

--- components/chess-game/ui/game-info.tsx
+++ components/chess-game/ui/game-info.tsx
@@ -4,16 +4,16 @@ import { UserIcon } from "./icons/user-icon"
 
 type props = {
 	isRatingGame?: boolean
-	playersCount?: number
+	playerCount?: number
 	timeMode: string
 }
 
-export function GameInfo({ isRatingGame, playersCount = 2, timeMode }: props) {
+export function GameInfo({ isRatingGame, playerCount = 2, timeMode }: props) {
 	return (
 		<div className="flex items-center gap-3 text-xs text-slate-400">
 			{isRatingGame && <StarIcon />}
 			<div className="flex items-center gap-1">
-				<UserIcon /> {playersCount}
+				<UserIcon /> {playerCount}
 			</div>
 			<div className="flex items-center gap-1">
 				<HistoryIcon /> {timeMode}
