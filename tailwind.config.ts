f741de6e1ff4905d968f5483a070b6743f1e9d42
SaD-n-SoliD <yodas-worker@yandex.ru>
2024-11-25 15:45:01 +0300

    added icon font

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,9 @@ const config: Config = {
 				"gradient-conic":
 					"conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
 			},
+			fontFamily: {
+				'chess-glyph': ['var(--icon-font-chess)']
+			},
 			lineHeight: {
 				tight: '1.2'
 			},
