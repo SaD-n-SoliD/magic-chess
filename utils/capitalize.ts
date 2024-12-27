683634ebc2f2f762df7cbe3a59a91ed6fee50fb7
SaD-n-SoliD <yodas-worker@yandex.ru>
2024-12-27 13:01:40 +0300

    added capitalize/decapitalize util

--- utils/capitalize.ts
+++ utils/capitalize.ts
@@ -1,4 +1,10 @@
 // Приводит первый символ строки к верхнему регистру
 export function capitalize(str: string) {
+	if (!str) return str
 	return str.charAt(0).toUpperCase() + str.slice(1)
+}
+// Приводит первый символ строки к нижнему регистру
+export function decapitalize(str: string) {
+	if (!str) return str
+	return str.charAt(0).toLowerCase() + str.slice(1)
 }
